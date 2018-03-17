6e1bb1a55f1ece1eb8f537429714bdab356ae337
Ryan Serpico <ryguy360@gmail.com>
2018-03-17 17:50:42 -0400

    It spits out html files now.

--- run2md.py
+++ run2md.py
@@ -7,11 +7,17 @@ input_file = io.open("python_md_test.md", mode="r", encoding="utf-8")
 text = input_file.read()
 content = markdown.markdown(text)
 
-@app.route("/")
-def template_test():
-	return render_template("template.html", **globals())
+# @app.route("/")
+# def template_test():
+# 	return render_template("template.html", **globals())
 	# Something needs to be added to this function so that it can spit out an HTML file. Each file name needs to have a different name. We could give each one a number that has 1 added to it each time the function runs. We could also set it up so that the first <h1> tag is the HTML file name.
 	# Doing the latter would allow for us to have the bot spit out the URL of the uploaded story.
+
+
+@app.route("/")
+def template_test():
+	with open('index.html', 'wb') as f:
+		return f.write(render_template('template.html', **globals()))
 
 # Open a connection with the FTP server and pass through the file that the previous function spat out.
 
